cb31540f1dd496f0b42af50101cdd64141d5c2b1
Shreya <goelshreya3001@gmail.com>
2026-04-17 23:03:08 +0530

    revisits this code

--- class code/StaticMethod.java
+++ class code/StaticMethod.java
@@ -1,12 +1,24 @@
-interface Shreya{
-    void say();
+// interface Shreya{
+//     void say();
+// }
+// public class StaticMethod{
+//     public static void says(){
+//         System.out.println("This is example of static method refrence");
+//     }
+//     public static void main(String[] args) {
+//         Shreya s=StaticMethod::says;
+//         s.say();
+//     }
+// }
+interface A {
+     public void getName(String Name);
 }
 public class StaticMethod{
-    public static void says(){
-        System.out.println("This is example of static method refrence");
-    }
-    public static void main(String[] args) {
-        Shreya s=StaticMethod::says;
-        s.say();
+        public static void getinfo(String Info ){
+            System.out.println(Info);
+        }
+         public static void main(String[] args) {
+            A a1= StaticMethod::getinfo;
+            a1.getName("Shreya");
     }
 }
