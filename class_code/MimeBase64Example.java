ea79d85838ead314e9969bb1edf4b85d9de93d26
Shreya <goelshreya3001@gmail.com>
2026-04-20 23:26:19 +0530

    mime eg

--- class_code/MimeBase64Example.java
+++ class_code/MimeBase64Example.java
@@ -1,5 +1,18 @@
-package class code;
+import java.util.Base64;
 
 public class MimeBase64Example {
-    
+    public static void main(String[] args) throws Exception {
+
+        String text = "Hello Shreya! This is a MIME Base64 encoding example in Java.";
+
+        // MIME Encoder (adds line breaks after 76 characters)
+        String encoded = Base64.getMimeEncoder().encodeToString(text.getBytes("UTF-8"));
+
+        // MIME Decoder
+        byte[] decodedBytes = Base64.getMimeDecoder().decode(encoded);
+        String decoded = new String(decodedBytes, "UTF-8");
+
+        System.out.println("Encoded (MIME):\n" + encoded);
+        System.out.println("\nDecoded:\n" + decoded);
+    }
 }
